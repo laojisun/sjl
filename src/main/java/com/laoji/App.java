6d234972145d4e10bab530d85cf3f265a46ef394
sunjilei <sunjilei@meione.cc>
2025-10-16 14:28:37 +0800

    孙纪雷 test

---
+++ src/main/java/com/laoji/App.java
@@ -0,0 +1,13 @@
+package com.laoji;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
